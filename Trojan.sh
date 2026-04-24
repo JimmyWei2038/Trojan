06d25b528a162b1cc12ca26297d99d88bf5369b9
Jimmy Wei <18672433344@163.com>
2026-04-24 16:04:45 +0800

    Update Trojan.sh

--- Trojan.sh
+++ Trojan.sh
@@ -234,5 +234,11 @@ blue "  xxx.xxx.xxx_key.key改名为private.pem"
 blue "	上传至/root/trojan/trojan-cert "
 red "	 执行 systemctl restart trojan 重启Trojan 服务"
 red "	 执行 systemctl status trojan  查看Trojan 运行状态"
+
+# 生成 Trojan 分享链接
+TROJAN_LINK="trojan://00000000@${your_domain}:443?sni=${your_domain}#${your_domain}-trojan"
+green "=========================================================="
+green "客户端分享链接（可直接导入 v2rayN）："
+red "${TROJAN_LINK}"
 green "=========================================="
 sleep 3s
